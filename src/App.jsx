499bfeb5e81d2edbdbb168fdf18845f86949a073
KamyaJohar02 <kamyajohar21999@gmail.com>
2023-12-18 23:36:30 +0530

    Business Card Copy

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+
+import './App.css'
+import Card from './Components/Card'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <Card />
+
+      </div>
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Card.jsx
@@ -0,0 +1,20 @@
+import "../App.css";
+
+const Card = () => {
+  return (
+    <div className="CardContainer">
+    
+      <div className="InternalContainer">       
+        <div className="PicContainer">
+          <div className="square1"></div>
+          <div className="square3"></div>
+          <div className="square2"></div>
+          <div className="circle"></div>
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
